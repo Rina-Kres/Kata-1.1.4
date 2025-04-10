c78a4815550f26f17edc9ac73e59c1eaa2ffa896
Rina Kres <podvecka@gmail.com>
2025-04-10 18:04:49 +0300

    Task_1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,41 +1,41 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoHibernateImpl();
+        UserService userService = new UserServiceImpl();
 
         try {
-            userDao.createUsersTable();
+            userService.createUsersTable();
             System.out.println("Таблица пользователей создана");
 
-            userDao.saveUser("Name1", "LastName1", (byte) 20);
+            userService.saveUser("Name1", "LastName1", (byte) 20);
             System.out.println("User с именем Name1 добавлен в базу данных");
-            userDao.saveUser("Name2", "LastName2", (byte) 25);
+            userService.saveUser("Name2", "LastName2", (byte) 25);
             System.out.println("User с именем Name2 добавлен в базу данных");
-            userDao.saveUser("Name3", "LastName3", (byte) 31);
+            userService.saveUser("Name3", "LastName3", (byte) 31);
             System.out.println("User с именем Name3 добавлен в базу данных");
-            userDao.saveUser("Name4", "LastName4", (byte) 38);
+            userService.saveUser("Name4", "LastName4", (byte) 38);
             System.out.println("User с именем Name4 добавлен в базу данных");
 
-            userDao.removeUserById(1);
+            userService.removeUserById(1);
             System.out.println("User с id 1 удален из базы данных");
 
-            List<User> users = userDao.getAllUsers();
+            List<User> users = userService.getAllUsers();
             System.out.println("Список пользователей:");
             for (User user : users) {
                 System.out.println(user);
             }
 
-            userDao.cleanUsersTable();
+            userService.cleanUsersTable();
             System.out.println("Таблица пользователей очищена");
 
-            userDao.dropUsersTable();
+            userService.dropUsersTable();
             System.out.println("Таблица пользователей удалена");
 
         } catch (Exception e) {
